d58726afce96a83cc462e74bbe2b930ca5017938
tektechnologies <info@tektechnologies.com>
2018-07-06 13:36:23 -0500

    adds hover menu for bottom nav bar and updated css

--- js/main.js
+++ js/main.js
@@ -1,37 +1,12 @@
-//(function() {
-//
-//  "use strict";
-//
-//  var taskItems = document.querySelectorAll(".task");
-//
-//  for ( var i = 0, len = taskItems.length; i < len; i++ ) {
-//    var taskItem = taskItems[i];
-//    contextMenuListener(taskItem);
-//  }
-//
-//  function contextMenuListener(el) {
-//    el.addEventListener( "contextmenu", function(e) {
-//      console.log(e, el);
-//    });
-//  }
-//
-//})();//
-//this is not yet working for the right click event for the header. 
-
-
-
-///////////////////////////////////////////////////////
-//this is the window scroll for daily notifications. weather?//
-///////////////////////////////////////////////////////
-window.onscroll = function() {myFunction()};
-
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
+ window.onscroll = function() {myFunction()};
+         
+         var header = document.getElementById("myHeader");
+         var sticky = header.offsetTop;
+         
+         function myFunction() {
+           if (window.pageYOffset > sticky) {
+             header.classList.add("sticky");
+           } else {
+             header.classList.remove("sticky");
+           }
+         }
